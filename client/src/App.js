b74c875e6cdafbe2841e9aef30d38dcd58ddd26a
Nenad <33637797+nenadperisic@users.noreply.github.com>
2020-07-28 18:04:47 +0200

    Revert "Added Log In pages for User and Driver"

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Register from './components/Register';
-import Login from './components/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Homepage} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/login" exact component={Login} />     
+        <Route path="/register" exact component={Register} />        
       </Switch>
     </BrowserRouter>
   );
