66e1c23ccf1ba5580c688d6b91c073b274633e15
DavidScepanovic <davidscepanovic96@gmail.com>
2020-08-25 16:52:15 +0200

    Added token and url services.

---
+++ server/Services/TokenService.cs
@@ -0,0 +1,74 @@
+﻿using CarGoSimulator.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarGoSimulator.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                ) ;
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = key,
+                ValidateLifetime = false 
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                return null;
+
+            return principal;
+        }
+    }
+}
---
+++ server/Services/UrlService.cs
@@ -0,0 +1,52 @@
+﻿using CarGoSimulator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CarGoSimulator.Services
+{
+    public class UrlService : IUrlService
+    {
+        public string PrepareString(string oldString)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(oldString.Trim(), @"\s+", "+");
+        }
+
+        public async Task<string> SendRequestAsync(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            string responseFromServer = null;
+
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                Stream data = response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(data);
+
+                responseFromServer = await reader.ReadToEndAsync();
+            }
+
+            return responseFromServer;
+        }
+
+        public string SendRequest(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            string responseFromServer = null;
+
+            using (WebResponse response = request.GetResponse())
+            {
+                Stream data = response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(data);
+
+                responseFromServer = reader.ReadToEnd();
+            }
+
+            return responseFromServer;
+        }
+    }
+}
